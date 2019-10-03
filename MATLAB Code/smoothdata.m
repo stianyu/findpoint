d6c9ba4b4b07fcec738ceebff3a3804d66b891f4
stianyu <shengtianyu_wh@163.com>
2019-10-03 19:03:40 +0800

    修改以适应矩阵的平滑

--- MATLAB Code/smoothdata.m
+++ MATLAB Code/smoothdata.m
@@ -1,24 +1,35 @@
-function smoothy = smoothdata(data, span) %#codegen
-n = length(data);
-smoothy = zeros(1921,1);
-for i = 1:n
-    s = 0;
-    if i<(span+1)/2
-        for j = 1:i
-            s = s + data(i-1+j);
+function smoothy = smoothdata(data, span, dim) %#codegen
+%MOVING AVERAGE FILTER. data could be a matrix, span should be a odd.
+m = size(data, 1); n = size(data, 2);
+if mod(span,2) == 0
+    span = span - 1;
+end
+if dim == 2
+    t = m;
+    m = n;
+    n = t;
+end
+smoothy = zeros(m, n);
+for l = 1:n
+    for i = 1:m
+        s = 0;
+        if i<(span+1)/2
+            for j = 1:i
+                s = s + data(i-1+j,l);
+            end
+            smoothy(i,l) = s/j;
+        elseif i>m-(span-1)/2
+            for j = i:m
+                s = s + data(j,l);
+            end
+            smoothy(i,l) = s/(m-i+1);
+        else
+            for j = 1:(span-1)/2
+                s = s +  data(i-j,l) + data(i+j,l);         
+            end
+            s = s + data(i,l);
+            smoothy(i,l) = s/span;
         end
-        smoothy(i) = s/j;
-    elseif i>n-(span-1)/2
-        for j = i:n
-            s = s + data(j);
-        end
-        smoothy(i) = s/(n-i+1);
-    else
-        for j = 1:(span-1)/2
-            s = s +  data(i-j) + data(i+j);         
-        end
-        s = s + data(i);
-        smoothy(i) = s/span;
     end
 end
 end
